5eca23dea218ccc100000790497e58883e454837
Henning Jacobs <henning@jacobs1.de>
2015-05-20 19:32:06 +0200

    fix syntax

--- csv.py
+++ csv.py
@@ -1,4 +1,4 @@
-#! usr/bin/env python3
+#!/usr/bin/env python3
 
 import json
 import sys
@@ -8,7 +8,7 @@ import os
 
 github_access_token = os.environ["GITHUB_ACCESS_TOKEN"]
 
-headers = { "AUTHORIZATION" : "token {}".format(github_access_token)}
+headers = { "Authorization" : "token {}".format(github_access_token)}
 
 github_base_url = "https://api.github.com/"
 
@@ -17,13 +17,13 @@ def lookup_team_id(email):
 
 def create_github_team(name, description):
     response = requests.post(
-        github_base_url + "orgs/zalando/teams", 
+        github_base_url + "orgs/zalando/teams",
         data = json.dumps({
-            "name" : name, 
+            "name" : name,
             "description": description,
             "permission": "admin"
             }),
-        headers = headers ) 
+        headers = headers )
     response.raise_for_status()
     return response.json()
 
@@ -36,10 +36,6 @@ with open(path) as file :
             github_base_url + "users/{}".format(github_username),
             headers = headers)
 
-        if user_response.status_code == 200
+        if user_response.status_code == 200:
             team_id = lookup_team_id(email)
             create_github_team(team_id)
-
-
-
-
