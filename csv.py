bc95e6c8559fb21b75e204980730d87824aa890f
Philipp Hofmann <philipp.hofmann@zalando.de>
2015-05-20 18:57:55 +0200

    implement create groups on github
    not tested, pizza first

--- csv.py
+++ csv.py
@@ -1,19 +1,45 @@
 #! usr/bin/env python3
 
+import json
 import sys
 import requests
 import os
 
+
 github_access_token = os.environ["GITHUB_ACCESS_TOKEN"]
+
+headers = { "AUTHORIZATION" : "token {}".format(github_access_token)}
+
+github_base_url = "https://api.github.com/"
+
+def lookup_team_id(email):
+    return 'stups'
+
+def create_github_team(name, description):
+    response = requests.post(
+        github_base_url + "orgs/zalando/teams", 
+        data = json.dumps({
+            "name" : name, 
+            "description": description,
+            "permission": "admin"
+            }),
+        headers = headers ) 
+    response.raise_for_status()
+    return response.json()
+
 
 path = sys.argv[1]
 with open(path) as file :
     for line in file :
         email, github_username = line.strip().split(',')
         user_response = requests.get(
-            "https://api.github.com/users/{}".format(github_username),
-            headers = { "AUTHORIZATION" : "token {}".format(github_access_token)})
+            github_base_url + "users/{}".format(github_username),
+            headers = headers)
 
-        print(user_response.status_code)
-        print(email, github_username)
+        if user_response.status_code == 200
+            team_id = lookup_team_id(email)
+            create_github_team(team_id)
+
+
+
 
