0e669c7fb4580e682860dfaf7d124e3dd3682b1a
Henning Jacobs <henning@jacobs1.de>
2015-05-20 20:53:28 +0200

    wrap actions in clickclick.Action

--- sync-github-teams.py
+++ sync-github-teams.py
@@ -6,6 +6,10 @@ import json
 import requests
 import zign.api
 
+from mock import MagicMock
+
+from clickclick import Action, info
+
 
 github_base_url = "https://api.github.com/"
 
@@ -14,32 +18,43 @@ github_base_url = "https://api.github.com/"
 @click.argument('csv_file')
 @click.argument('team_service_url')
 @click.option('--github-access-token', envvar='GITHUB_ACCESS_TOKEN')
-def cli(csv_file, team_service_url, github_access_token):
+@click.option('--dry-run', is_flag=True)
+def cli(csv_file, team_service_url, github_access_token, dry_run):
     # we just assume we got a valid token
     access_token = zign.api.get_existing_token('team-service')
     access_token = access_token['access_token']
 
     headers = {'Authorization': 'Bearer {}'.format(access_token)}
-    r = requests.get(team_service_url + '/teams', headers=headers)
 
-    uid_to_team = {}
-    teams_with_members = set()
+    with Action('Collecting team memberships from team service..') as act:
+        r = requests.get(team_service_url + '/teams', headers=headers)
 
-    for team in r.json():
-        resp = requests.get(team_service_url + '/teams/{}'.format(team['id']), headers=headers)
-        info = resp.json()
-        for member in info['members']:
-            uid_to_team[member] = info['id']
-            teams_with_members.add(info['id'])
+        uid_to_team = {}
+        teams_with_members = set()
+
+        for team in r.json():
+            resp = requests.get(team_service_url + '/teams/{}'.format(team['id']), headers=headers)
+            act.progress()
+            data = resp.json()
+            for member in data['members']:
+                uid_to_team[member] = data['id']
+                teams_with_members.add(data['id'])
 
     headers = {"Authorization": "token {}".format(github_access_token)}
 
     def lookup_team_id(email):
         return 'stups'
 
+    def request(func, url, **kwargs):
+        if dry_run:
+            print('**DRY-RUN** {} {}'.format(func, url))
+            return MagicMock()
+        else:
+            return func(url, **kwargs)
+
     def create_github_team(name):
         description = '{} team'.format(name)
-        response = requests.post(
+        response = request(requests.post,
             github_base_url + "orgs/zalando/teams",
             data=json.dumps({
                 "name": name,
@@ -52,7 +67,7 @@ def cli(csv_file, team_service_url, github_access_token):
         if errors:
             for error in errors:
                 if error.get('code') == 'already_exists':
-                    print('team already exists')
+                    info('team already exists')
                     return
         response.raise_for_status()
         return response.json()
@@ -65,7 +80,11 @@ def cli(csv_file, team_service_url, github_access_token):
         return teams_by_name
 
     def add_github_team_member(team: dict, username: str):
-        r = requests.put(github_base_url + 'teams/{}/memberships/{}'.format(team['id'], username), headers=headers)
+        r = request(requests.put, github_base_url + 'teams/{}/memberships/{}'.format(team['id'], username), headers=headers)
+        r.raise_for_status()
+
+    def remove_github_team_member(team: dict, username: str):
+        r = request(requests.delete, github_base_url + 'teams/{}/memberships/{}'.format(team['id'], username), headers=headers)
         r.raise_for_status()
 
     def get_github_team_members(team: dict):
@@ -79,26 +98,29 @@ def cli(csv_file, team_service_url, github_access_token):
     with open(csv_file) as file:
         for line in file:
             email, github_username = line.strip().split(',')
-            user_response = requests.get(
-                github_base_url + "users/{}".format(github_username),
-                headers=headers)
+            with Action('Checking GitHub user {}..'.format(github_username)):
+                user_response = requests.get(
+                    github_base_url + "users/{}".format(github_username),
+                    headers=headers)
 
-            if user_response.status_code == 200:
-                team_id = lookup_team_id(email)
-                create_github_team(team_id)
-                github_teams = get_github_teams()
-                github_team = github_teams.get(team_id)
-                add_github_team_member(github_team, github_username)
-                users_by_team[github_team['id']].add(github_username)
+                if user_response.status_code == 200:
+                    team_id = lookup_team_id(email)
+                    create_github_team(team_id)
+                    github_teams = get_github_teams()
+                    github_team = github_teams.get(team_id)
+                    add_github_team_member(github_team, github_username)
+                    users_by_team[github_team['id']].add(github_username)
 
     github_teams = get_github_teams()
     for team_id, github_team in github_teams.items():
         if team_id not in teams_with_members:
             continue
-        github_members = get_github_team_members(github_team)
-        team_members = users_by_team[github_team['id']]
-        members_to_be_removed = github_members - team_members
-        print(team_id, members_to_be_removed)
+        with Action('Removing members of team {}..'.format(team_id)):
+            github_members = get_github_team_members(github_team)
+            team_members = users_by_team[github_team['id']]
+            members_to_be_removed = github_members - team_members
+            for member in members_to_be_removed:
+                remove_github_team_member(github_team, member)
 
 if __name__ == '__main__':
     cli()
