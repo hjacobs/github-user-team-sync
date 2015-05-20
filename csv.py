3e528dd7d0920a68a3c943938c1d9e60e102a496
Henning Jacobs <henning@jacobs1.de>
2015-05-20 18:28:36 +0200

    retab

--- csv.py
+++ csv.py
@@ -7,13 +7,13 @@ import os
 github_access_token = os.environ["GITHUB_ACCESS_TOKEN"]
 
 path = sys.argv[1]
-with open(path) as file : 
-	for line in file : 
-		email, github_username = line.strip().split(',')
-		user_response = requests.get(
-			"https://api.github.com/users/{}".format(github_username), 
-			headers = { "AUTHORIZATION" : "token {}".format(github_access_token)})
-		
-		print(user_response.status_code)
-		print(email, github_username) 
+with open(path) as file :
+    for line in file :
+        email, github_username = line.strip().split(',')
+        user_response = requests.get(
+            "https://api.github.com/users/{}".format(github_username),
+            headers = { "AUTHORIZATION" : "token {}".format(github_access_token)})
+
+        print(user_response.status_code)
+        print(email, github_username)
 
