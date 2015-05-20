575420084fa8c6a1b51dfb7d694a3826d789c52e
Philipp Hofmann <philipp.hofmann@zalando.de>
2015-05-20 18:23:58 +0200

    add support for github access token
    more api requests per minute like this

--- csv.py
+++ csv.py
@@ -2,14 +2,18 @@
 
 import sys
 import requests
+import os
 
-
+github_access_token = os.environ["GITHUB_ACCESS_TOKEN"]
 
 path = sys.argv[1]
 with open(path) as file : 
 	for line in file : 
 		email, github_username = line.strip().split(',')
-		user_response = requests.get("https://api.github.com/users/{}".format(github_username))
+		user_response = requests.get(
+			"https://api.github.com/users/{}".format(github_username), 
+			headers = { "AUTHORIZATION" : "token {}".format(github_access_token)})
+		
 		print(user_response.status_code)
-			#print(email, github_username) 
+		print(email, github_username) 
 
