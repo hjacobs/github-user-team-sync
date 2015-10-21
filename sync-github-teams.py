584901ca982a2d603fa68a8668f115308f3c72ee
Henning Jacobs <henning@jacobs1.de>
2015-10-21 17:50:41 +0200

    connection pooling, GitHub pagination

--- sync-github-teams.py
+++ sync-github-teams.py
@@ -2,6 +2,7 @@
 
 import click
 import collections
+import itertools
 import json
 import requests
 import zign.api
@@ -12,6 +13,11 @@ from clickclick import Action, info
 
 
 github_base_url = "https://api.github.com/"
+
+sess = requests.Session()
+adapter = requests.adapters.HTTPAdapter(pool_connections=20, pool_maxsize=20)
+sess.mount('https://', adapter)
+requests = sess
 
 
 def read_csv_file(csv_file):
@@ -26,14 +32,34 @@ def read_csv_file(csv_file):
         yield github_username, uid
 
 
+def get_member_teams(team_service_url, access_token):
+    headers = {'Authorization': 'Bearer {}'.format(access_token)}
+
+    with Action('Collecting team memberships from team service..') as act:
+        r = requests.get(team_service_url + '/teams', headers=headers)
+        r.raise_for_status()
+
+        uid_to_teams = collections.defaultdict(set)
+
+        for team in r.json():
+            if team['id']:
+                resp = requests.get(team_service_url + '/teams/{}'.format(team['id']), headers=headers)
+                act.progress()
+                data = resp.json()
+                for member in data.get('member', []):
+                    uid_to_teams[member].add(data['id'])
+    return uid_to_teams
+
+
 @click.command()
 @click.argument('csv_file', type=click.File('r'))
 @click.argument('team_service_url')
 @click.option('--github-access-token', envvar='GITHUB_ACCESS_TOKEN', help='GitHub personal access token', metavar='TOKEN')
 @click.option('--dry-run', is_flag=True, help='No-op: do not modify anything, just show what would be done')
 @click.option('--no-remove', is_flag=True, help='Do not remove any team members')
+@click.option('--filter', help='Only process matching GitHub usernames')
 @click.option('--team-service-token-name', default='team-service', help='Zign OAuth token name to use', metavar='NAME')
-def cli(csv_file, team_service_url, github_access_token, dry_run: bool, no_remove: bool, team_service_token_name):
+def cli(csv_file, team_service_url, github_access_token, dry_run: bool, no_remove: bool, team_service_token_name, filter: str):
     '''
     Synchronize users and team memberships with GitHub.com.
 
@@ -49,22 +75,10 @@ def cli(csv_file, team_service_url, github_access_token, dry_run: bool, no_remov
 
     users = list(read_csv_file(csv_file))
 
-    headers = {'Authorization': 'Bearer {}'.format(access_token)}
+    uid_to_teams = get_member_teams(team_service_url, access_token)
 
-    with Action('Collecting team memberships from team service..') as act:
-        r = requests.get(team_service_url + '/teams', headers=headers)
-        r.raise_for_status()
-
-        uid_to_teams = collections.defaultdict(set)
-        teams_with_members = set()
-
-        for team in r.json():
-            resp = requests.get(team_service_url + '/teams/{}'.format(team['id']), headers=headers)
-            act.progress()
-            data = resp.json()
-            for member in data.get('member', []):
-                uid_to_teams[member].add(data['id'])
-                teams_with_members.add(data['id'])
+    teams_with_members = set(itertools.chain(*uid_to_teams.values()))
+    info('Found {} users in {} teams'.format(len(uid_to_teams), len(teams_with_members)))
 
     headers = {"Authorization": "token {}".format(github_access_token)}
 
@@ -96,18 +110,24 @@ def cli(csv_file, team_service_url, github_access_token, dry_run: bool, no_remov
         return response.json()
 
     def get_github_teams():
-        # TODO: needs to support pagination
-        r = requests.get(github_base_url + 'orgs/zalando/teams', params={'per_page': 100}, headers=headers)
         teams_by_name = {}
-        for team in r.json():
-            teams_by_name[team['name']] = team
+        page = 1
+        while True:
+            r = requests.get(github_base_url + 'orgs/zalando/teams', params={'per_page': 100, 'page': page}, headers=headers)
+            for team in r.json():
+                teams_by_name[team['name']] = team
+            page += 1
+            if 'next' not in r.headers.get('Link'):
+                break
         return teams_by_name
 
     def add_github_team_member(team: dict, username: str):
+        info('Adding {} to {}..'.format(username, team['name']))
         r = request(requests.put, github_base_url + 'teams/{}/memberships/{}'.format(team['id'], username), headers=headers)
         r.raise_for_status()
 
     def remove_github_team_member(team: dict, username: str):
+        info('Removing {} from {}..'.format(username, team['name']))
         r = request(requests.delete, github_base_url + 'teams/{}/memberships/{}'.format(team['id'], username), headers=headers)
         r.raise_for_status()
 
@@ -120,6 +140,8 @@ def cli(csv_file, team_service_url, github_access_token, dry_run: bool, no_remov
     users_by_team = collections.defaultdict(set)
 
     for github_username, uid in users:
+        if filter and filter.lower() not in github_username.lower():
+            continue
         with Action('Checking GitHub user {}..'.format(github_username)) as act:
             user_response = requests.get(
                 github_base_url + "users/{}".format(github_username),
@@ -132,7 +154,7 @@ def cli(csv_file, team_service_url, github_access_token, dry_run: bool, no_remov
                     github_teams = get_github_teams()
                     github_team = github_teams.get(team_id)
                     if not github_team:
-                        act.error('no GitHub team')
+                        act.error('no GitHub team: {}'.format(team_id))
                         continue
                     add_github_team_member(github_team, github_username)
                     users_by_team[github_team['id']].add(github_username)
